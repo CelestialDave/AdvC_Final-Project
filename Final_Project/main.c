83395354a4637aceb986b42b3fa886a09a5fb101
noam <noamwa95@gmail.com>
2018-06-08 16:19:43 +0300

    last

--- Final_Project/main.c
+++ Final_Project/main.c
@@ -1,21 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "declerations.h"
 int main() {
-	/*add-an-apt "Dizengoff 180 Tel Aviv" 2000000 4 28 04 16
-add-an-apt "Rothschild 67 Rishon Le Zion" 1700000 3 03 10 16
-add-an-apt "Ben Gurion 25 Herzliya" 200000 5 01 08 16
-add-an-apt "Gordon 85 Holon" 1500000 3 15 06 17
-get-an-apt –MinimumNumRooms 0
-delete-an-apt -Enter 1
-*/
-
 	char* command;
 	History_Data hData;
 	historyDataPreLoaderPrep(&hData);
 	// Data Loader() 
 	readDataFromFiles(&hData);
-
-
 	// Opening Message:
 	printf("Please enter one of the following commands:\nadd-an-apt, get-an-apt, buy-an-apt or delete-an-apt\nFor reconstruction commands, please enter:\n!!, !num, history, short_history or !num^str1^str2\nTo exit, enter exit.\n");
 	command = getCommand();
